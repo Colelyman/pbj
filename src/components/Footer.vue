573457abd01bd68e46829e26448d46d06260ea2d
Cole Lyman <cole@colelyman.com>
2018-02-19 20:45:55 -0700

    Added transitions to the questions.

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,8 @@
 <template>
   <div class="footer">
     <transition
-      name="question-transition"
-      mode="out-in">
-      <p>{{ currentQuestion.q }}</p>
+      name="fade">
+      <p :key="qChange">{{ currentQuestion.q }}</p>
     </transition>
   </div>
 </template>
@@ -19,11 +18,12 @@ export default {
                 { q: 'Is PB and J on toasted bread still a PB&J?' },
                 { q: 'Is PB and J on pumpernickel or gluten-free bread considered a PB&J?' },
                 { q: 'On which side should a PB&J land when dropped?' },
-                { q: 'If a PB&J falls in the forest, and there\' no one there to eat it, is it still a PB&J?' },
+                { q: 'If a PB&J falls in the forest, and there\'s no one there to eat it, is it still a PB&J?' },
                 { q: 'Is dipping bread in PB and J still a PB&J?' },
                 { q: 'What is the allowable tolerance of peanuts in chunky PB?' },
             ],
-            currentQuestion: {}
+            currentQuestion: {},
+            qChange: false
         }
     },
     created: function() {
@@ -32,6 +32,7 @@ export default {
         var cycle = function() {
             let rand = Math.floor(Math.random() * self.questions.length)
             self.currentQuestion = self.questions[rand]
+            self.qChange = !self.qChange
             setTimeout(cycle, interval)
         }
         cycle()
@@ -40,6 +41,18 @@ export default {
 </script>
 
 <style scoped>
-
+.footer {
+        height: 100px;
+}
+.fade-enter-active {
+    transition: opacity .3s ease;
+}
+.fade-leave-active {
+    transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
+}
+.fade-enter, .fade-leave-to {
+    transform: translateX(10px);
+    opacity: 0;
+}
 </style>
 
