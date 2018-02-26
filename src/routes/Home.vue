1081809fa0a271121c94d224614fbf7feab288db
Cole Lyman <cole@colelyman.com>
2018-02-26 04:54:25 -0700

    Added the figures and the captions.

--- src/routes/Home.vue
+++ src/routes/Home.vue
@@ -27,11 +27,11 @@
   </div>
   <h3>The forms of the PB&amp;J</h3>
   <div class="row">
-    <p class="twelve columns">A strength of the PB&amp;J is that it is adaptable to the partaker’s specific taste, as well as the ingredients that are available. One example of the versatility of the PB&amp;J is found in Figure 1, where the grain substrate is two Ritz crackers. However, this strength can quickly become a weakness when the variations of the PB&amp;J become extreme.</p> 
+    <p class="twelve columns">A strength of the PB&amp;J is that it is adaptable to the partaker’s specific taste, as well as the ingredients that are available. One example of the versatility of the PB&amp;J is found in Figure 1, where the grain substrate is two Ritz crackers. However, this strength can quickly become a weakness when the variations of the PB&amp;J become extreme, illustrated by Figure 7.</p> 
   </div>
-  <slider animation="fade" :auto="true" height="420px">
+  <slider animation="fade" :auto="true" :interval="4500" height="420px">
     <slider-item v-for="(image, i) in this.images" :key="i">
-      <img :src="image.src">
+      <img :src="image.src" height="300px">
       <p class="caption"><strong>Figure {{ i + 1 }}:</strong> {{ image.caption }} <em>Source:</em> <a class="reference" :href="image.ref">{{ image.ref }}</a></p>
     </slider-item>
   </slider> 
@@ -53,6 +53,41 @@ export default {
                     caption: 'This Ritz cracker PB&J shows the versatility of the sandwich, and fulfills the proper criteria to still be considered a PB&J.',
                     ref: 'http://www.snackworks.com/recipe/pbj-ritzwich-188689.aspx'
                 },
+                {
+                    src: 'images/monteCristo.jpg',
+                    caption: 'What is better than PB&J? A stack of two PB&J, coated in Monte Cristo batter, and fried!',
+                    ref: 'http://www.ohbiteit.com/2013/10/pbj-monte-cristo.html'
+                },
+                {
+                    src: 'images/waffle.png',
+                    caption: 'Why not make the most American meal an option for breakfast as well?',
+                    ref: 'https://www.smuckers.com/recipes/waffle-pbj-8492'
+                },
+                {
+                    src: 'images/classic.jpg',
+                    caption: 'You can never beat the classic.',
+                    ref: 'http://sierraclub.typepad.com/compass/2010/11/interview-fighting-climate-change-with-pbj.html'
+                },
+                {
+                    src: 'images/quesadilla.jpg',
+                    caption: 'This is for those times when there is no bread, but there is still a need for PB&J. In this case, the grain based substrate is a tortilla.',
+                    ref: 'http://redtri.com/easy-lunch-ideas-and-recipes/slide/30'
+                },
+                {
+                    src: 'images/grilled.jpg',
+                    caption: 'If you can grill chees, you can grill PB&J.',
+                    ref: 'https://potatorolls.com/recipes/grilled-pbj-sandwich/'
+                },
+                {
+                    src: 'images/sausage.png',
+                    caption: 'This is an example that does not fit the PB&J criteria, but it is noteworth to acknowledge the creativity of infusing bratwurst with PB&J.',
+                    ref: 'https://www.lakegenevacountrymeats.com/blog/leahys-sausage/peanut-butter-jelly-bratwurst'
+                },
+                {
+                    src: 'images/smore.jpg',
+                    caption: 'In addition to meeting all the criteria for a PB&J sandwich, this s\'more goes the extra mile in making the sandwich extra sweet.',
+                    ref: 'https://www.bettycrocker.com/recipes/pb-j-smore/1fa33a97-7205-46ca-b01a-b47df289b328'
+                }
             ]
         }
     },
