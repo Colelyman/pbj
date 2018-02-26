551f01a404644ef22f83f014fd4fb61a3c95cdd8
Cole Lyman <cole@colelyman.com>
2018-02-26 10:02:14 -0700

    Fixed figure number.

--- src/routes/Fun-facts.vue
+++ src/routes/Fun-facts.vue
@@ -6,10 +6,10 @@
       <li>Maybe one of the reasons that the PB&amp;J is such a big hit is that it has roots in our nation with Thomas Jefferson and Jimmy Carter, two former presidents, being peanut farmers. They may have been the ones that promoted National Peanut Butter Day, which takes place on February 12th, and National Jelly Day, which takes placed on April 2nd.</li>
       <li>The popularity of PB&amp;J continues to grow. In recent years the world’s largest PB&amp;J sandwich was made as part of the Great American Peanut Butter Festival in Grand Saline, TX. This mammoth sandwich weighed 1,342 pounds.</li>
       <li>There is no reason to be worried about peanut butter shortages with its rise in popularity, a single acre can contain enough peanuts to make 30,000 PB&amp;J sandwiches.</li>
-      <li>George Washington Carver (see Figure 8) is often credited with the invention of peanut butter. Although he developed more than 300 uses for peanuts, it is debated whether peanut butter was one of these. Regardless, he certainly improved the world through peanut awareness!</li>
+      <li>George Washington Carver (see Figure 9) is often credited with the invention of peanut butter. Although he developed more than 300 uses for peanuts, it is debated whether peanut butter was one of these. Regardless, he certainly improved the world through peanut awareness!</li>
     </ul>
     <img src="/images/gc.jpg" width="90%" />
-    <p><strong>Figure 8</strong> Photo of George Washington carver in his laboratory. <em>Source:</em> <a href="http://teacher.scholastic.com/activities/bhistory/inventors/carver.htm">http://teacher.scholastic.com/activities/bhistory/inventors/carver.htm</a></p>
+    <p><strong>Figure 9</strong> Photo of George Washington carver in his laboratory. <em>Source:</em> <a href="http://teacher.scholastic.com/activities/bhistory/inventors/carver.htm">http://teacher.scholastic.com/activities/bhistory/inventors/carver.htm</a></p>
   </div>
 </template>
 
